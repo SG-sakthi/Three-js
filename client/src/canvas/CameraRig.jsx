a2b22bc716510eb30dce6b15f4d96f3edb84bbf3
sakthiganesh <sakthiganesh782@gmail.com>
2023-11-08 19:19:46 +0530

    first commit

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Customizer = () => {
+  return(
+  <div></div>
+  )
+}
+
+export default Customizer
---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import {Environment,Center} from '@react-three/drei'
+
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+
+const CanvasModel = () => {
+  return(
+<div></div>
+    )
+}
+
+export default CanvasModel
